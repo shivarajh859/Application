6fe5b2d4fe4b5c38919b544b6ff82210b04305ff
shivarajh859 <shivarajh1234555@gmail.com>
2025-03-20 19:04:17 +0530

    Delete samplejavaproject directory

--- samplejavaproject/src/com/gentech/stringassignment/q5.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.stringassignment;
-
-public class q5 {
-    public static void main(String[] args) {
-        String str = "SUNDAYMONDAYTUESDAYWEDNESDAYTHURSDAYFRIDAYSATURDAY";
-        String newStr = str.replace("DAY"," ");      
-        System.out.println(newStr);
-    }
-}
