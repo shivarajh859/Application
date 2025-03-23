c15a76a4f55493cee99a7dfbd89225706e1de699
shivu <shivaraj1234555@gmail.com>
2025-03-23 19:30:25 +0530

    Initial commit

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
-    app.run(host='0.0.0.0', port=port)
+        port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+        app.run(host='0.0.0.0', port=port, debug=True)
